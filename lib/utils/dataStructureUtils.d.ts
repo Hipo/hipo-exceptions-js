1242a03f2261f0eb7b9962b240691c53eab38e98
Berkan Unal <Berkanunal@gmail.com>
2020-02-26 18:46:32 +0300

    chore(git): Add lib to gitignore and package.json file property

--- lib/utils/dataStructureUtils.d.ts
+++
@@ -1,9 +0,0 @@
-declare function createMapFromObject(obj: {
-    [key: string]: any;
-}): Map<string, any>;
-declare function isArrayOfStrings(x: unknown): x is string[];
-declare function isArrayOfObjects(x: unknown): x is Array<{
-    [key: string]: any;
-}>;
-declare function isObjectEmpty(obj: unknown): obj is {};
-export { createMapFromObject, isArrayOfStrings, isArrayOfObjects, isObjectEmpty };
