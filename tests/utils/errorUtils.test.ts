d65d51723613adf91b38557aaff40b484d5eee8f
Berkan Unal <Berkanunal@gmail.com>
2020-02-27 19:50:19 +0300

    chore(error-utils): Add tests for getErrorDetail
    Related Issues: #5

--- tests/utils/errorUtils.test.ts
+++ tests/utils/errorUtils.test.ts
@@ -1,4 +1,8 @@
-import { generateMessageFromStringArray } from "../../src/utils/errorUtils";
+import {
+  generateMessageFromStringArray,
+  getErrorDetail
+} from "../../src/utils/errorUtils";
+import { Exception } from "../../src/ExceptionTransformerModel";
 
 describe("generateMessageFromStringArray", () => {
   const mockStringArray = ["name", "password", "email"];
@@ -53,5 +57,103 @@ describe("generateMessageFromStringArray", () => {
       expect(message).not.toBe("key: name");
       expect(message).toBe("key: ");
     });
+  });
+});
+
+describe("getErrorDetail", () => {
+  it("should return null when given undefined or null", () => {
+    const undefinedResult = getErrorDetail(undefined);
+    const nullResult = getErrorDetail(null);
+
+    expect(undefinedResult).toBe(null);
+    expect(nullResult).toBe(null);
+  });
+
+  it("should return empty object when detail is an empty object", () => {
+    const mockError = {
+      type: "ValidationError",
+      detail: {},
+      fallback_message: ""
+    };
+    const detail = getErrorDetail(mockError);
+
+    expect(detail).toStrictEqual({});
+  });
+
+  it("should be able to handle 'complex detail array with objects'", () => {
+    const mockError: Exception = {
+      type: "ValidationError",
+      detail: {
+        non_field_errors: [
+          { address: ["The address entered is not valid."] },
+          { phone_number: ["The phone number entered is not valid."] }
+        ]
+      },
+      fallback_message: "This is a random fallback message"
+    };
+
+    const detail = getErrorDetail(mockError);
+
+    expect(detail).toStrictEqual({
+      non_field_errors: [
+        { address: ["The address entered is not valid."] },
+        { phone_number: ["The phone number entered is not valid."] }
+      ]
+    });
+  });
+
+  it("should be able to handle 'complex detail array with (some) empty objects'", () => {
+    const mockErrorWithSomeEmptyObjects: Exception = {
+      type: "ValidationError",
+      detail: {
+        non_field_errors: [
+          {},
+          {},
+          { address: ["The address entered is not valid."] },
+          { phone_number: ["The phone number entered is not valid."] }
+        ]
+      },
+      fallback_message: "This is a random fallback message"
+    };
+    const mockErrorWithOnlyEmptyObjects: Exception = {
+      type: "ValidationError",
+      detail: {
+        non_field_errors: [{}, {}]
+      },
+      fallback_message: "This is a random fallback message"
+    };
+
+    const detailWithSomeEmptyObjects = getErrorDetail(
+      mockErrorWithSomeEmptyObjects
+    );
+    const detailWithOnlyEmptyObjects = getErrorDetail(
+      mockErrorWithOnlyEmptyObjects
+    );
+
+    expect(detailWithSomeEmptyObjects).toStrictEqual({
+      non_field_errors: [
+        {},
+        {},
+        { address: ["The address entered is not valid."] },
+        { phone_number: ["The phone number entered is not valid."] }
+      ]
+    });
+    expect(detailWithOnlyEmptyObjects).toStrictEqual({
+      non_field_errors: [{}, {}]
+    });
+  });
+
+  it("should return detail object correctly when given an ordinary error", () => {
+    const mockError = {
+      type: "ValidationError",
+      detail: {
+        email: ["Enter a valid email address."],
+        password: ["Enter a valid password address."]
+      },
+      fallback_message: "This is a random fallback message"
+    };
+    const detail = getErrorDetail(mockError);
+
+    expect(detail).toStrictEqual(mockError.detail);
   });
 });
